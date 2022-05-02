1d56bb5381012212510617fc8ded662fe839f452
Matypinkman <55607185+Matypinkman@users.noreply.github.com>
2022-05-01 23:17:58 -0400

    Se actualiza Cliente del API REST

---
+++ ApiRest Servicio/src/main/java/com/example/demo/RestApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiServiceApplication.class, args);
+	}
+
+}
